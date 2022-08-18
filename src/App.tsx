70b180435ec17f2be8f4302691c2d1daf4aca0a7
sanyalu <sanyalu97@gmail.com>
2022-08-18 14:24:46 +0200

    11. grids

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,43 @@
 import React from 'react';
 import './App.css';
-import Header from "./Header";
-import Technologies from "./Technologies";
 
 function App() {
     return (
-        <div className="App">
-            <Header />
-            <Technologies />
+        <div className='container'>
+            <div className="app">
+                <header className="header">
+                    <img className='logo' src="https://upload.wikimedia.org/wikipedia/commons/5/53/Wikimedia-logo.png" alt=""/>
+                </header>
+
+                <nav className="nav">
+                    <ul>
+                        <li>Profile</li>
+                        <li>Messages</li>
+                        <li>News</li>
+                        <li>Music</li>
+                        <li>Settings</li>
+                    </ul>
+                </nav>
+
+                <div className="content">
+                    <div>
+                        <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg" alt=""
+                            className='content-img'/>
+                    </div>
+                    <div>
+                        ava + description
+                    </div>
+                    <div>
+                        My posts
+                        <div>
+                            New post
+                        </div>
+                        <div>post 1</div>
+                        <div>post 2</div>
+                    </div>
+
+                </div>
+            </div>
         </div>
     );
 }
