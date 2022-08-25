6d9bc5cea095296f698ab7efe3af00517eff7e78
sanyalu <sanyalu97@gmail.com>
2022-08-25 21:41:19 +0200

    react router + dialogs layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 import './App.css';
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
-import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
+import {Route} from "react-router-dom";
+import Profile from "./components/Profile/Profile";
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Settings from "./components/Settings/Settings";
 
 function App() {
     return (
@@ -12,8 +16,22 @@ function App() {
                 <Header/>
                 <Navbar/>
                 <div className="main">
-                    {/*<Profile/>*/}
-                    <Dialogs/>
+                    <Route path={'/profile'}>
+                        <Profile/>
+                    </Route>
+                    <Route path={'/dialogs'}>
+                        <Dialogs/>
+                    </Route>
+                    <Route path={'/news'}>
+                        <News/>
+                    </Route>
+                    <Route path={'/music'}>
+                        <Music/>
+                    </Route>
+                    <Route path={'/settings'}>
+                        <Settings/>
+                    </Route>
+
                 </div>
             </div>
         </div>
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,15 +1,36 @@
 import React from 'react';
 import styles from '../../cssModules/Navbar.module.css'
+import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
     return (
         <nav className={styles.nav}>
             <ul>
-                <li className={`${styles.item} ${styles.activeItem}`}>Profile</li>
-                <li className={styles.item}>Messages</li>
-                <li className={styles.item}>News</li>
-                <li className={styles.item}>Music</li>
-                <li className={styles.item}>Settings</li>
+                <li className={styles.item}>
+                    <NavLink to={'/profile'} activeClassName={styles.activeItem}>
+                        Profile
+                    </NavLink>
+                </li>
+                <li className={styles.item}>
+                    <NavLink to={'/dialogs'} activeClassName={styles.activeItem}>
+                        Messages
+                    </NavLink>
+                </li>
+                <li className={styles.item}>
+                    <NavLink to={'/news'} activeClassName={styles.activeItem}>
+                        News
+                    </NavLink>
+                </li>
+                <li className={styles.item}>
+                    <NavLink to={'/music'} activeClassName={styles.activeItem}>
+                        Music
+                    </NavLink>
+                </li>
+                <li className={styles.item}>
+                    <NavLink to={'/settings'} activeClassName={styles.activeItem}>
+                        Settings
+                    </NavLink>
+                </li>
             </ul>
         </nav>
     );
