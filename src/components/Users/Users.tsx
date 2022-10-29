1084280a92a315166194c3385d250315db6c4cb2
sanyalu <sanyalu97@gmail.com>
2022-10-29 18:03:19 +0200

    62. follow/unfollow

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -2,56 +2,89 @@ import React from 'react';
 import defaultUserAvatar from "../../assets/images/avatar.png";
 import {UserType} from "../../redux/users-reducer";
 import {NavLink} from "react-router-dom";
-
+import axios from "axios";
 
 
 type PropsType = {
-  users: UserType[],
-  countOnPage: number
-  currentPage: number
-  totalCount: number
-  follow: (userId: string) => void
-  unfollow: (userId: string) => void
-  handlePageChange: (currentPage: number) => void
+    users: UserType[],
+    countOnPage: number
+    currentPage: number
+    totalCount: number
+    follow: (userId: string) => void
+    unfollow: (userId: string) => void
+    handlePageChange: (currentPage: number) => void
 }
 
 const Users:React.FC<PropsType> = (props) => {
 
-  const pagesCount = Math.ceil(props.totalCount / props.countOnPage)
-  const pages = []
-  for (let i = 1; i <= pagesCount; i++) {
-    pages.push(<span
-      key={i}
-      style={{
-        marginRight: '5px',
-        cursor: 'pointer',
-        color: i === props.currentPage ? '#ffd184' : 'inherit'
-      }}
-      onClick={() => props.handlePageChange(i)}>{i}</span>)
-  }
+    const pagesCount = Math.ceil(props.totalCount / props.countOnPage)
+    const pages = []
+    for (let i = 1; i <= pagesCount; i++) {
+        pages.push(<span
+            key={i}
+            style={{
+                marginRight: '5px',
+                cursor: 'pointer',
+                color: i === props.currentPage ? '#ffd184' : 'inherit'
+            }}
+            onClick={() => props.handlePageChange(i)}>{i}</span>)
+    }
 
 
-  return (
-    <div style={{padding: '10px'}}>
-      <div style={{marginBottom: '10px', overflowX: 'scroll', width: '500px'}}>
-        {pages}
-      </div>
-      <ul>
-        {props.users.map(u => {
-          return (
-            <li key={u.id} style={{marginBottom: '10px', paddingBottom: '10px', borderBottom: '1px solid white'}}>
-              <NavLink to={`/profile/${u.id}`}><img src={u.photos.small ? u.photos.small : defaultUserAvatar}
-                      style={{width: '48px', height: '48px', borderRadius: '50%'}}/></NavLink>
-              <div>Name: {u.name}</div>
-              {u.followed
-                ? <button onClick={() => props.unfollow(u.id)}>Unfollow</button>
-                : <button onClick={() => props.follow(u.id)}>Follow</button>}
-            </li>
-          )
-        })}
-      </ul>
-    </div>
-  )
+    return (
+        <div style={{padding: '10px'}}>
+            <div style={{marginBottom: '10px', overflowX: 'scroll', height: '30px', width: '500px'}}>
+                {pages}
+            </div>
+            <ul>
+                {props.users.map(u => {
+                    return (
+                        <li key={u.id}
+                            style={{marginBottom: '10px', paddingBottom: '10px', borderBottom: '1px solid white'}}>
+                            <NavLink to={`/profile/${u.id}`}><img
+                                src={u.photos.small ? u.photos.small : defaultUserAvatar}
+                                style={{width: '48px', height: '48px', borderRadius: '50%'}}/></NavLink>
+                            <div>Name: {u.name}</div>
+                            {u.followed
+                                ? <button onClick={() => {
+                                    axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {
+                                        withCredentials: true,
+                                        headers: {
+                                            'API-KEY': 'bf0a45fd-7838-4bec-9b72-3f203f5ee23d'
+                                        }
+                                    })
+                                        .then(res => {
+                                            if (res.data.resultCode === 0) {
+                                                props.unfollow(u.id)
+                                            } else {
+                                                console.warn(res.data.messages[0])
+                                            }
+                                        })
+
+                                }}>Unfollow</button>
+                                : <button onClick={() => {
+                                    axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, null, {
+                                        withCredentials: true,
+                                        headers: {
+                                            'API-KEY': 'bf0a45fd-7838-4bec-9b72-3f203f5ee23d'
+                                        }
+                                    })
+                                        .then(res => {
+                                            if (res.data.resultCode === 0) {
+                                                props.follow(u.id)
+                                            } else {
+                                                console.warn(res.data.messages[0])
+                                            }
+                                        })
+
+
+                                } }>Follow</button>}
+                        </li>
+                    )
+                })}
+            </ul>
+        </div>
+    )
 };
 
 export default Users;
--- src/components/Users/UsersContainer.tsx
+++ src/components/Users/UsersContainer.tsx
@@ -3,91 +3,95 @@ import Users from "./Users";
 import React from "react";
 import axios from "axios";
 import {
-  follow,
-  setCurrentPage,
-  setIsFetching,
-  setTotalCount,
-  setUsers,
-  unfollow,
-  UserType
+    follow,
+    setCurrentPage,
+    setIsFetching,
+    setTotalCount,
+    setUsers,
+    unfollow,
+    UserType
 } from "../../redux/users-reducer";
 import {StateType} from "../../redux/redux-store";
 import {Dispatch} from "redux";
 
 
 type MapStateToPropsType = {
-  users: UserType[]
-  countOnPage: number
-  currentPage: number
-  totalCount: number
-  isFetching: boolean
+    users: UserType[]
+    countOnPage: number
+    currentPage: number
+    totalCount: number
+    isFetching: boolean
 }
 
 export type UsersPropsType = MapStateToPropsType & {
-  follow: (userId: string) => void
-  unfollow: (userId: string) => void
-  setUsers: (users: UserType[]) => void
-  setCurrentPage: (currentPage: number) => void
-  setTotalCount: (totalCount: number) => void
-  setIsFetching: (isFetching: boolean) => void
+    follow: (userId: string) => void
+    unfollow: (userId: string) => void
+    setUsers: (users: UserType[]) => void
+    setCurrentPage: (currentPage: number) => void
+    setTotalCount: (totalCount: number) => void
+    setIsFetching: (isFetching: boolean) => void
 }
 
 
 class UsersContainer extends React.Component<UsersPropsType, any> {
 
-  componentDidMount() {
-    this.props.setIsFetching(true)
-    axios.get(`https://social-network.samuraijs.com/api/1.0/users/?count=${this.props.countOnPage}&page=${this.props.currentPage}`).then(response => {
-      this.props.setUsers(response.data.items)
-      this.props.setTotalCount(response.data.totalCount)
-      this.props.setIsFetching(false)
-    })
-  }
+    componentDidMount() {
+        this.props.setIsFetching(true)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users/?count=${this.props.countOnPage}&page=${this.props.currentPage}`, {
+            withCredentials: true
+        })
+            .then(response => {
+                this.props.setUsers(response.data.items)
+                this.props.setTotalCount(response.data.totalCount)
+                this.props.setIsFetching(false)
+            })
+    }
 
-  handlePageChange = (page: number) => {
-    this.props.setIsFetching(true)
-    axios.get(`https://social-network.samuraijs.com/api/1.0/users/?count=${this.props.countOnPage}&page=${page}`).then(response => {
-      this.props.setUsers(response.data.items)
-      this.props.setCurrentPage(page)
-      this.props.setTotalCount(response.data.totalCount)
-      this.props.setIsFetching(false)
-    })
-  }
+    handlePageChange = (page: number) => {
+        this.props.setIsFetching(true)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users/?count=${this.props.countOnPage}&page=${page}`, {
+            withCredentials: true
+        })
+            .then(response => {
+                this.props.setUsers(response.data.items)
+                this.props.setCurrentPage(page)
+                this.props.setTotalCount(response.data.totalCount)
+                this.props.setIsFetching(false)
+            })
+    }
 
-  render() {
-    return (
-      this.props.isFetching
-        ? <h2>Loading...</h2>
-        : <Users
-          users={this.props.users}
-          countOnPage={this.props.countOnPage}
-          currentPage={this.props.currentPage}
-          totalCount={this.props.totalCount}
-          follow={this.props.follow}
-          unfollow={this.props.unfollow}
-          handlePageChange={this.handlePageChange}/>
-    )
-  }
+    render() {
+        return (
+            this.props.isFetching
+                ? <h2>Loading...</h2>
+                : <Users
+                    users={this.props.users}
+                    countOnPage={this.props.countOnPage}
+                    currentPage={this.props.currentPage}
+                    totalCount={this.props.totalCount}
+                    follow={this.props.follow}
+                    unfollow={this.props.unfollow}
+                    handlePageChange={this.handlePageChange}/>
+        )
+    }
 }
 
 
 const mapStateToProps = (state: StateType): MapStateToPropsType => ({
-  users: state.usersPage.users,
-  countOnPage: state.usersPage.countOnPage,
-  currentPage: state.usersPage.currentPage,
-  totalCount: state.usersPage.totalCount,
-  isFetching: state.usersPage.isFetching
+    users: state.usersPage.users,
+    countOnPage: state.usersPage.countOnPage,
+    currentPage: state.usersPage.currentPage,
+    totalCount: state.usersPage.totalCount,
+    isFetching: state.usersPage.isFetching
 
 })
 
 
-
-
 export default connect(mapStateToProps, {
-  follow,
-  unfollow,
-  setUsers,
-  setCurrentPage,
-  setTotalCount,
-  setIsFetching
+    follow,
+    unfollow,
+    setUsers,
+    setCurrentPage,
+    setTotalCount,
+    setIsFetching
 })(UsersContainer)
