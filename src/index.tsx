c060df73509ea80d8db5951dbd7ee43e2644f082
sanyalu <sanyalu97@gmail.com>
2023-01-18 15:52:36 +0100

    95. deploy to github pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,16 @@
-import React from 'react';
-import './index.css';
-import ReactDOM from "react-dom";
-import {BrowserRouter} from "react-router-dom";
-import App from "./App";
-import store from "./redux/redux-store";
-import {Provider} from "react-redux";
+import React from 'react'
+import './index.css'
+import ReactDOM from 'react-dom'
+import { HashRouter } from 'react-router-dom'
+import App from './App'
+import store from './redux/redux-store'
+import { Provider } from 'react-redux'
 
-
-
-const rerenderEntireTree = () => {
-    ReactDOM.render(
-        <BrowserRouter>
-            <Provider store={store}>
-                <App/>
-            </Provider>
-        </BrowserRouter>,
-        document.getElementById('root')
-    );
-}
-
-rerenderEntireTree()
-
-store.subscribe(rerenderEntireTree);
-
+ReactDOM.render(
+  <HashRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </HashRouter>,
+  document.getElementById('root'),
+)
